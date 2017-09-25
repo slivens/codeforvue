7cd74c54fa820d16736677ddb27742e2cfe0afe9
Hemiah <309595700@qq.com>
2017-09-25 14:40:03 +0800

    clear

--- my-blog/src/routes.js
+++
@@ -1,8 +0,0 @@
-import ShowBlogs from './components/ShowBlogs.vue'
-import AddBlog from './components/AddBlog.vue'
-import SingleBlog from './components/SingleBlog.vue'
-export default[
-	{path:"/",component:ShowBlogs},
-	{path:"/add",component:AddBlog},
-	{path:"/blog/:id",component:SingleBlog}
-]
